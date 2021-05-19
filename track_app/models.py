55c63a63b4f9e31a2f820111a4ec1415e040a081
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-05-19 09:08:22 -0400

    Update models.py

--- track_app/models.py
+++ track_app/models.py
@@ -7,9 +7,15 @@ class Recruiter(models.Model):
     last_name = models.CharField(max_length=30)
     position = models.CharField(max_length=30)
     commission = models.FloatField()
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Placement(models.Model):
-
+    
+    position_title = models.CharField(max_length=30)
+    company_name = models.CharField(max_length=30)
     placement_revenue = models.FloatField()
     client_find = models.CharField(max_length=30)
     client_find_revenue = models.FloatField()
@@ -19,3 +25,6 @@ class Placement(models.Model):
     candidate_find_revenue = models.FloatField()
     candidate_owner = models.CharField(max_length=30)
     candidate_owner_revenue = models.FloatField()
+    
+    def __str__(self):
+        return self.company_name + " - " + self.position_title
